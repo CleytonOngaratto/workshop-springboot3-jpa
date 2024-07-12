9b242586334d88036206bc7a1dc40ec60e215e98
CleytonOngaratto <cleytongarattosousa@gmail.com>
2024-07-12 19:37:29 -0300

    Project created

---
+++ src/test/java/com/webservicespring/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservicespring.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
