991ea479d05f51e6fe2ede6f88af141d66ec9366
CleytonOngaratto <cleytongarattosousa@gmail.com>
2024-07-23 21:06:03 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/webservicespring/project/entities/Product.java
+++ src/main/java/com/webservicespring/project/entities/Product.java
@@ -1,5 +1,6 @@
 package com.webservicespring.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,8 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name = "product_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();  //O uso de set ao invés de list é pra informar pro JPA não admitir repetições do mesmo item
 
     public Product() {
 
@@ -79,6 +82,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     @Override
